f4770f07a288bf2b026c77a683fbb3a5286b5e55
minchodang <minsu910725@gmail.com>
2024-11-21 10:38:58 +0900

    Feat: react 통합 및 yarn, prettier 셋팅

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 
+declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
+declare const MAIN_WINDOW_VITE_NAME: string;
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
