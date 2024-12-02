5910681c066a6b670194bfe2788d76ea23c5316e
minchodang <minsu910725@gmail.com>
2024-12-02 10:55:56 +0900

    Feat: router-dom 셋팅

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,12 @@
-import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
 
 export default function App() {
   return (
-    <>
-      <h1>Hello, world!</h1>
-      <Counter />
-    </>
-  );
-}
-
-function Counter() {
-  const [count, setCount] = useState(0);
-  return (
-    <button className="bg-red-800 text-red-500" onClick={() => setCount(count + 1)}>
-      You clicked me {count} times
-    </button>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
